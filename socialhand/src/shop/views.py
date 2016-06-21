9469f0fa55cec2bf069daddd3f483f0ae9283708
GonzaloAvilez <g_avilez_ig@hotmail.com>
2016-06-21 00:52:10 -0500

    shop: global_userview, templates and main.css for catalogue

--- socialhand/src/shop/views.py
+++ socialhand/src/shop/views.py
@@ -5,7 +5,8 @@ from cart.forms import CartAddProductForm
 def product_list(request, category_slug=None):
 	category = None
 	categories = Category.objects.all()
-	products = Product.objects.filter(author=request.user)
+	products = Product.objects.filter(available=all)
+	# products = Product.objects.filter(author=request.user) by author
 	if category_slug:
 		category = get_object_or_404(Category, slug=category_slug)
 		products = products.filter(category=category)
