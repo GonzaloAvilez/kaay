7c3e09269c5f5beb67370399cdb4a7e8ced995ac
GonzaloAvilez <g_avilez_ig@hotmail.com>
2016-08-23 19:16:39 -0500

    Testings diferent solutions for pagination CBV before to implement

--- socialhand/src/socialhand/settings/base.py
+++ socialhand/src/socialhand/settings/base.py
@@ -9,13 +9,18 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 from django.core.urlresolvers import reverse_lazy
 from os.path import dirname, join, exists
+ 
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
+TEMPLATE_CONTEXT_PROCESSORS += (
+    'django.core.context_processors.request',
+)   
 
 # Build paths inside the project like this: join(BASE_DIR, "directory")
 BASE_DIR = dirname(dirname(dirname(__file__)))
 STATICFILES_DIRS = [join(BASE_DIR, 'static')]
 
 
-
+EL_PAGINATION_PER_PAGE=9
 
 # Use Django templates using the new Django 1.8 TEMPLATES settings
 TEMPLATES = [
@@ -80,7 +85,10 @@ INSTALLED_APPS = (
     'authtools',
     'crispy_forms',
     'easy_thumbnails',
-
+    
+    'el_pagination',
+    'infinite_pagination',
+    'endless_pagination',
     'social.apps.django_app.default',
     'embed_video',  
     'paypal.standard.ipn',
