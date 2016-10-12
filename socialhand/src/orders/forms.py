aa6abe48366012d2073a9fee03e284cacddaa3d6
GonzaloAvilez <g_avilez_ig@hotmail.com>
2016-10-12 01:41:46 -0500

    cart-table responsive

--- socialhand/src/orders/forms.py
+++ socialhand/src/orders/forms.py
@@ -2,9 +2,24 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from .models import Order
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
+from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 
-
+ 
 class OrderCreateForm(forms.ModelForm):
+	def __init__ (self, *args, **kwargs):
+		super(OrderCreateForm,self).__init__ (*args,**kwargs)
+		self.helper = FormHelper()
+		self.helper.form_tag = False
+		self.helper.layout = Layout (
+			Field('first_name'),
+			Field('last_name'),
+			Field('email'),
+			Field('address'),
+			Field('postal_code'),
+			Field('city'),
+			)
 	
 	class Meta:
 		model = Order
