e38f34465e404c94fdec2f032341483d1785b6c6
GonzaloAvilez <g_avilez_ig@hotmail.com>
2016-10-28 23:24:04 -0500

    image optimization for loading speed

--- socialhand/src/socialhand/views.py
+++ socialhand/src/socialhand/views.py
@@ -1,8 +1,9 @@
 from django.views import generic
-
-from django.shortcuts import render_to_response
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response,redirect
 from .forms import ContactForm 
+from django.template import Context
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
 
 # Elasticsearch 
 # from .forms import PostSearchForm
@@ -10,10 +11,42 @@ from .forms import ContactForm
 
 class HomePage(generic.TemplateView):
     template_name = "home.html"
+    http_method_names = ['get', 'post']
+
 
     def get(self,request,*args,**kwargs):
     	form_class = ContactForm
+    	
+
     	kwargs['form'] = form_class
+    	return super(HomePage,self).get(request,*args,**kwargs)
+    
+    def post(self,request,*args,**kwargs):
+    	form_class = ContactForm
+    	kwargs['form'] = form_class
+    	if request.method == 'POST':
+    		form = form_class(data=request.POST)
+	    	if form.is_valid():
+	    		subject = request.POST.get('subject','')
+	    		contact_name = request.POST.get('contact_name','')
+	    		contact_email = request.POST.get('contact_email','')
+	    		content = request.POST.get('content','')
+	    		template = get_template('contact_template.txt')
+	    		context = Context({
+	    			'subject':subject,
+	    			'contact_name':contact_name,
+	    			'contact_email':contact_email,
+	    			'content':content,
+	    			})
+	    		content = template.render(context)
+	    		email = EmailMessage(
+	    			"New contact form submission",
+	    			content,
+	    			"ikaay.com.mx"+'',
+	    			['g.avilez.ig@gmail.com'],
+	    			headers = {'Reply to':contact_email})
+	    		email.send()
+	    		# return redirect('get')
     	return super(HomePage,self).get(request,*args,**kwargs)
 
 
